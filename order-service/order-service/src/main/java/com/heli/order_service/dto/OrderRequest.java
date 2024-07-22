919bdd50c2d5d85ea392e37d2a02e40e4793cb96
HansakaHeli <programminghhr66@gmail.com>
2024-07-22 19:51:21 +0530

    Integrate Kafka with Schema Registry

--- order-service/order-service/src/main/java/com/heli/order_service/dto/OrderRequest.java
+++
@@ -1,6 +0,0 @@
-package com.heli.order_service.dto;
-
-import java.math.BigDecimal;
-
-public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
-}
